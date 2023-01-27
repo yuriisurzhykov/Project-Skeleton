553901b7f092d0ef765ebe9c162590eda47e888d
Yurii Surzhykov <yurii.surzhykov@gcpsoftware.com>
2023-01-27 04:36:41 +0200

    Create core module and connect it to project

---
+++ core/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = ProjectConfigs.applicationId
+    compileSdk = ProjectConfigs.compileVersion
+
+    defaultConfig {
+        minSdk = ProjectConfigs.minSdkVersion
+        targetSdk = ProjectConfigs.targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "Project-Skeleton"
 include(":app")
+include(":core")
